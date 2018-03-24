8185d9ff256e88ca8e3c9cd0dba89cf99d7da177
ruslantuchkov <nidjel87@gmail.com>
2018-03-24 13:13:23 +0500

    added some packages and libraries

---
+++ src/Routes.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from './components/Home/Home';
+
+class Routes extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    );
+  }
+}
+
+export default Routes;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './Routes';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
