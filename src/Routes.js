f817e21292eb1ae2668b31b3c5623779e79e0d5f
ruslantuchkov <nidjel87@gmail.com>
2018-03-24 17:38:09 +0500

    layout component and before eject

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import Layout from './hoc/Layout/Layout';
 import Home from './components/Home/Home';
 
 class Routes extends Component {
   render() {
     return (
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Layout>
     );
   }
 }
